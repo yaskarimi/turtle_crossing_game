0111ed0258a4b610fb9e70e070bde71780ac3bb7
ykarimi <ykarimi.2312@gmail.com>
2021-03-09 15:53:43 -0800

    final commit

--- score.py
+++ score.py
@@ -9,16 +9,17 @@ class Scoreboard(Turtle):
         self.penup()
         self.hideturtle()
         self.level = LEVEL
-        self.setposition(-270, 260)
-        self.write(f"level:{self.level}", False, font=FONT)
+        self.goto(-250, 260)
+        self.write(f"level:{self.level}", False,align="center", font=FONT)
 
     def next_level(self):
         self.clear()
         self.level += 1
-        self.write(f"level:{self.level}", False, font=FONT)
+        self.write(f"level:{self.level}", False, align="center",font=FONT)
 
     def game_over(self):
         self.clear()
-        self.write("Game over", False, font=FONT)
-        self.penup()
         self.goto(0, 0)
+        self.write("Game over", False,align="center", font=FONT)
+        self.penup()
+
